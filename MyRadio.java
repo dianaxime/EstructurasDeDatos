fb94b2aedad5a4980fb49097a90af2a914602773
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-01-16 00:24:29 -0600

    Add files via upload

---
+++ MyRadio.java
@@ -0,0 +1,43 @@
+/* Clase MyRadio
+
+ * Hoja de Trabajo 1 de Algoritmos y Estructuras de Datos
+
+ * Utilizacion de mi radio de un automovil
+
+ * 16 de enero de 2019
+
+ * @autor:
+
+    Diana Ximena de Leon Figueroa 18607
+
+    Fatima Jazmin Albeño Barrios 18060
+
+*/
+
+public class MyRadio () {
+
+	public static void main(String[] args) {
+
+		UseRadio radio;
+
+		radio = new UseRadio();
+
+		int opcion;
+
+		opcion = 0;
+
+		while ( opcion=!7 ) {
+
+			System.out.println( "---------------------------------------" );
+			System.out.println( "| 1. Encender el radio                |" );
+			System.out.println( "| 2. Cambiar de AM a FM y viceversa   |" );
+			System.out.println( "| 3. Cambiar de emisora               |" );
+			System.out.println( "| 4. Guardar una emisora              |" );
+			System.out.println( "| 5. Seleccionar una emisora guardada |" );
+			System.out.println( "| 6. Apagar el radio                  |" );
+			System.out.println( "| 7. Salir                            |" );
+			System.out.println( "---------------------------------------" );
+		}
+		
+	}
+}
